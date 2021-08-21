cf254afd7044d5ffb67ce115896bdbb9aca105c1
sbjang <sbjang123456@gmail.com>
2021-08-21 19:37:13 +0900

    justify-content space-between -> center 로 변경

--- packages/client/src/components/calendar/CalendarDate.tsx
+++ packages/client/src/components/calendar/CalendarDate.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { lPad, getDateFormatting, changeMonthYmd } from 'utils/dateUtils';
 import { classNames } from 'utils';
 
@@ -22,13 +22,16 @@ const CalendarDate = ({
                           onScheduleClick
                       }: CalendarDateProps): React.ReactElement => {
 
-    const handleClickDate = (sDate: string[], type: string) => () => {
-        const formattingDate = getDateFormatting(
-            type === 'ne' ? changeMonthYmd(sDate, 'next') :
-                type === 'be' ? changeMonthYmd(sDate, 'prev') : sDate
-        );
-        onDateClick(formattingDate);
+    const handleClickDate = (dateFormatting: string) => () => {
+        onDateClick(dateFormatting);
     };
+
+    const getDateFormattingByType = useCallback((arrDate: string[], type: string) => {
+        return getDateFormatting(
+            type === 'ne' ? changeMonthYmd(arrDate, 'next') :
+                type === 'be' ? changeMonthYmd(arrDate, 'prev') : arrDate
+        );
+    }, []);
 
     const calendarDays = useMemo(() => {
         const year = Number(sYear);
@@ -64,10 +67,10 @@ const CalendarDate = ({
         <>
             {calendarDays.map((week: any, idxWeek) => (
                 <div key={idxWeek} role='row' className='Date'>
-                    {week.map((day: any, idx: number) => {
-                        const dateKey = getDateFormatting([sYear, sMonth, lPad(day.date)]);
+                    {week.map((day: any) => {
+                        const dateKey = getDateFormattingByType([sYear, sMonth, lPad(day.date)], day.type);
                         return (
-                            <div key={dateKey} onClick={handleClickDate([sYear, sMonth, lPad(day.date)], day.type)}>
+                            <div key={dateKey} onClick={handleClickDate(dateKey)}>
                                 <span className={classNames('dateLabel', day.type !== 'cu' && 'notCurrentMonth')}>
                                     <span className={classNames(dateKey === getDateFormatting(today) && 'today')}>
                                         {day.date}
@@ -75,8 +78,8 @@ const CalendarDate = ({
                                 </span>
                                 {schedules
                                     .filter((schedule) => (
-                                        schedule.startAt.substr(0, 10) === dateKey ||
-                                        schedule.endAt.substr(0, 10) === dateKey
+                                        schedule.startAt.substr(0, 10) <= dateKey &&
+                                        schedule.endAt.substr(0, 10) >= dateKey
                                     ))
                                     .sort()
                                     .map((schedule) => {
